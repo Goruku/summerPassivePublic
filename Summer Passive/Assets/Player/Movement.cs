7097510ded2d220890fc959d57d8ebcd4264d7a7
Goruku <41842302+Goruku@users.noreply.github.com>
2022-05-01 22:18:33 -0400

    Refactor out generic functions

--- Summer Passive/Assets/Player/Movement.cs
+++ Summer Passive/Assets/Player/Movement.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+using Player;
 using Player.Ability;
-using Unity.VisualScripting;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class Movement : MonoBehaviour {
     
@@ -17,12 +13,12 @@ public class Movement : MonoBehaviour {
 
     private KeyCode[] movementKeys = { KeyCode.W, KeyCode.D, KeyCode.S, KeyCode.A };
     
-    private readonly Dictionary<KeyCode, KeyInfo> _keyInfo= new ();
+    private readonly Dictionary<KeyCode, ControlUtility.KeyInfo> _keyInfo= new ();
 
     // Start is called before the first frame update
     void Start() {
         foreach (KeyCode key in movementKeys) {
-            _keyInfo.Add(key, new KeyInfo(0));
+            _keyInfo.Add(key, new ControlUtility.KeyInfo(0));
         }
     }
 
@@ -40,44 +36,22 @@ public class Movement : MonoBehaviour {
         }
         dash.Trigger();
     }
-    
-    private static Vector3 GetDirection(KeyCode keyCode) {
-        return keyCode switch {
-            KeyCode.W => Vector3.up,
-            KeyCode.D => Vector3.right,
-            KeyCode.S => Vector3.down,
-            KeyCode.A => Vector3.left,
-            _ => Vector3.zero
-        };
-    }
 
     private void HeldKeyMove(KeyCode keyCode, float magnitude) {
         if (Input.GetKey(keyCode))
-            transform.position += GetDirection(keyCode) * magnitude;
+            transform.position += ControlUtility.GetDirection(keyCode) * magnitude;
     }
 
     private void DetectDashInput(KeyCode keyCode) {
         if (Input.GetKeyDown(keyCode)) {
-            KeyInfo keyInfo = _keyInfo[keyCode];
+            ControlUtility.KeyInfo keyInfo = _keyInfo[keyCode];
             float currentTime = Time.time;
             if (currentTime - keyInfo.Timestamp <= doubleTapWindow && lastPressed == keyCode) {
-                dash.direction = GetDirection(keyCode);
+                dash.direction = ControlUtility.GetDirection(keyCode);
                 dash.Arm();
             }
             keyInfo.Update();
             lastPressed = keyCode;
-        }
-    }
-    
-    private class KeyInfo {
-        public float Timestamp;
-
-        public KeyInfo(float time) {
-            this.Timestamp = time;
-        }
-
-        public void Update() {
-            Timestamp = Time.time;
         }
     }
 
