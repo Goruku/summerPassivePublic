776f763270d4ec88dbafabb45a9c7e4780df8b93
Goruku <41842302+Goruku@users.noreply.github.com>
2022-07-09 20:02:45 -0400

    simplified ObservableList

--- Summer Passive/Assets/Passive/PassiveTreeEntry.cs
+++ Summer Passive/Assets/Passive/PassiveTreeEntry.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -10,6 +12,8 @@ namespace Passive {
     [RequireComponent(typeof(PassiveTree))]
     [ExecuteAlways]
     public class PassiveTreeEntry : MonoBehaviour {
+
+        private bool enableOnce;
 
         [SerializeField, GetSet("active")]
         private bool _active;
@@ -21,33 +25,44 @@ namespace Passive {
                 if (!enabled) return;
                 foreach (var node in passiveNodes) {
                     node.neighbourNeeded += _active ? -1 : 1;
-                    PrefabUtility.RecordPrefabInstancePropertyModifications(node);
                 }
             }
         }
 
         [Serialize]
         public ObservableList<PassiveNode> passiveNodes = new ();
-        
-        void Start() {
-            passiveNodes.ItemAdded += node => {
-                SwitchOnWithActive(node, enabled);
-            };
-            passiveNodes.ItemRemoved += node => {
-                SwitchOffWithActive(node, enabled);
-            };
+
+        private void ListenToChange(object caller, NotifyCollectionChangedEventArgs e) {
+            if (e.NewItems != null)
+                foreach (PassiveNode node in e.NewItems) {
+                    if (!node) continue;
+                    SwitchOnWithActive(node, enabled);
+                }
+            if (e.OldItems != null)
+                foreach (PassiveNode node in e.OldItems) {
+                    if (!node) continue;
+                    SwitchOffWithActive(node, enabled);
+                }
         }
 
         private void OnEnable() {
+            if (enableOnce) return;
+            passiveNodes.CollectionChanged += ListenToChange;
             foreach (var node in passiveNodes) {
                 SwitchOnWithActive(node);
             }
+            
+            enableOnce = true;
         }
 
         private void OnDisable() {
+            if (!enableOnce) return;
+            passiveNodes.CollectionChanged -= ListenToChange;
             foreach (var node in passiveNodes) {
                 SwitchOffWithActive(node);
             }
+
+            enableOnce = false;
         }
         
         void Update()
@@ -58,18 +73,16 @@ namespace Passive {
         private void SwitchOnWithActive(PassiveNode node, bool guardFalse = true) {
             if (!guardFalse) return;
             node.neighbourNeeded += active ? -1 : 0;
-            PrefabUtility.RecordPrefabInstancePropertyModifications(node);
         }
         
         private void SwitchOffWithActive(PassiveNode node, bool guardFalse = true) {
             if (!guardFalse) return;
             node.neighbourNeeded += active ? 1 : 0;
-            PrefabUtility.RecordPrefabInstancePropertyModifications(node);
         }
     }
 
     [Serializable]
-    public class ObservableList<T> : IList<T>, INotifyCollectionChanged<T>, 
+    public class ObservableList<T> : ObservableCollection<T>, 
         #if UNITY_EDITOR
         ISerializationCallbackReceiver 
         #endif
@@ -79,35 +92,36 @@ namespace Passive {
         [SerializeField]
         private List<T> serializedListInterface = new();
         #endif
-        private List<T> _list = new ();
-        
+            
+            
+            
         private Queue<T> _changeAdded = new();
         private Queue<T> _changeRemoved = new();
-        
-        public event Action<T> ItemAdded = new Action<T>(obj => {});
-        public event Action<T> ItemRemoved = new Action<T>(obj => {});
-        public event Action CollectionChanged = new Action(() => { });
 
-        
+        private IList<T> _items;
+        protected new IList<T> Items {
+            get => _items;
+            set => _items = value;
+        }
         #if UNITY_EDITOR
         public void OnBeforeSerialize() {
             serializedListInterface.Clear();
-            serializedListInterface = new List<T>(_list);
+            serializedListInterface = new List<T>(Items);
         }
         
         public void OnAfterDeserialize() {
             
-            foreach (var item in _list) {
+            foreach (var item in this) {
                 if (!serializedListInterface.Contains(item)) {
                     _changeRemoved.Enqueue(item);
                 }
             }
             foreach (var item in serializedListInterface) {
-                if (!_list.Contains(item)) {
+                if (!Contains(item)) {
                     _changeAdded.Enqueue(item);
                 }
             }
-            _list = new List<T>(serializedListInterface);
+            Items = new List<T>(serializedListInterface);
         }
         #endif
 
@@ -115,12 +129,11 @@ namespace Passive {
             if (_changeAdded.Count <= 0 && _changeRemoved.Count <= 0) return ;
             
             while (_changeAdded.Count > 0) {
-                ItemAdded(_changeAdded.Dequeue());
+                Add(_changeAdded.Dequeue());
             }
             while (_changeRemoved.Count > 0) {
-                ItemRemoved(_changeRemoved.Dequeue());
+                Remove(_changeRemoved.Dequeue());
             }
-            CollectionChanged();
         }
 
         public T[] GetDiffAdded() {
@@ -129,64 +142,6 @@ namespace Passive {
 
         public T[] GetDiffRemoved() {
             return _changeRemoved.ToArray();
-        }
-
-        public void Add(T item) {
-            _list.Add(item);
-            ItemAdded(item);
-            CollectionChanged.Invoke();
-        }
-
-        public bool Remove(T item) {
-            var result = _list.Remove(item);
-            ItemRemoved(item);
-            CollectionChanged.Invoke();
-            return result;
-        }
-
-        public void Insert(int index, T item) {
-            _list.Insert(index, item);
-            ItemAdded.Invoke(item);
-            CollectionChanged.Invoke();
-        }
-
-        public void RemoveAt(int index) {
-            var item = _list[index];
-            _list.RemoveAt(index);
-            ItemRemoved(item);
-            CollectionChanged.Invoke();
-        }
-
-        public void Clear() {
-            foreach (var item in _list) {
-                Remove(item);
-            }
-        }
-
-        public bool Contains(T item) { return _list.Contains(item); }
-
-        public void CopyTo(T[] array, int arrayIndex) { _list.CopyTo(array, arrayIndex); }
-
-        public int Count { get => _list.Count; }
-
-        //TODO: wtf
-        public bool IsReadOnly { get => false; }
-
-        public IEnumerator<T> GetEnumerator() { return _list.GetEnumerator(); }
-        IEnumerator IEnumerable.GetEnumerator() { return _list.GetEnumerator(); }
-
-        public int IndexOf(T item) { return _list.IndexOf(item); }
-
-        public T this[int index]
-        {
-            get => _list[index];
-            set {
-                var item = _list[index];
-                _list[index] = value;
-                ItemRemoved(item);
-                ItemAdded(value);
-                CollectionChanged.Invoke();
-            }
         }
     }
 
